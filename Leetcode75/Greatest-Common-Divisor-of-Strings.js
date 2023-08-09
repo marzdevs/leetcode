908674ce5534c8f71971e7722dc45044df20d57a
Marzdevs <alzokarimarwa@gmail.com>
2023-08-09 13:58:41 -0500

    Create Greatest-Common-Divisor-of-Strings.js

---
+++ Leetcode75/Greatest-Common-Divisor-of-Strings.js
@@ -0,0 +1,21 @@
+/*
+return largest string x 
+x divides both strings
+*/
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 != str2 + str1){
+        return ""
+    }
+
+    
+
+};
+// Example usage
+const str1 = "ABCABC";
+const str2 = "ABC";
+console.log(gcdOfStrings(str1, str2));  // Output: "ABC"
