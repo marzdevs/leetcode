9cc97bab0a612c54d5ac8ec10ef23dc3e1923bdd
Marzdevs <alzokarimarwa@gmail.com>
2023-10-13 13:20:14 -0500

    Greatest candy

---
+++ Leetcode75/Array/python/GreatestNumofCandy.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+        max_candy = max(candies)
+        for i in range(len(candies)):
+            candies[i] = candies[i] + extraCandies >= max_candy
+        return candies
+
